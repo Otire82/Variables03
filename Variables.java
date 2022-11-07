7027c4ce0241c3924336c7a97af0a028afa204a2
Otire82 <a.jeanne01@sncf.fr>
2022-11-07 14:25:21 +0100

    quest03

---
+++ Variables.java
@@ -0,0 +1,17 @@
+/**
+ * Variables
+ */
+public class Variables {
+
+    public static void main(String...args) {
+        String title = "Indiana Jones and the Last Crusade";
+        boolean movie = true;
+        int year = 1989;
+        double score = 8.2;
+
+    System.out.println(title);
+    System.out.println(movie);
+    System.out.println(year);
+    System.out.println(score);
+    }
+}
